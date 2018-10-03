603104e35b4ccd5dd06da89ee016ad1138e30df6
Annie Cheng <38330264+az-cheng@users.noreply.github.com>
2018-10-03 19:00:30 -0400

    Add files via upload

---
+++ hidden.py
@@ -0,0 +1,15 @@
+
+
+# Keep this file separate
+
+# def oauth() :
+#     return { "consumer_key" : "2pd5WYH6wEkJ8Je5sux9F2l7G",
+#         "consumer_secret" : "xp21i0NqlXOkuDK2CqQYXXcEDMLUMgpqFdUTX1oV0lLKeYDLJ2",
+#         "token_key" : "2227811952-0iHFgphunHtlO8u1nebeUgL5TA8IPRpLfAvK8IP",
+#         "token_secret" : "dPLYLbQia6lqFXNa8JA6fccc1iFdfJLMVmPPuNal29lqT"}
+
+def oauth() :
+    return { "consumer_key" : "srHcccePX0asW8OTRWS2iZ0u6",
+        "consumer_secret" : "bF6sKeBdqpVeAX85Z1r9wyvDlcL8FLqbvmg7SM4cE1sJzFR6eh",
+        "token_key" : "847880217586683904-OK8dH7IVb41BoQ6JVg7jVjObB2hv4qa",
+        "token_secret" : "IhDGz8FRYKkfzY5FyrdXrKRmEKv5kMYZIQ2LRLV8Gwa1r" }
